398566f6ebd880fb2a367d4eb99c3cffc2593cc3
duchq183888 <hqd162@gmail.com>
2021-08-10 13:18:17 +0700

    add factorial function

---
+++ SolutionInternT8/Lesson2/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int Fact(int n) {
+	if (n == 0||n==1) return 1;
+	int a = 0;
+	int rs = 1;        
+	while (a < n) {
+		a = a + 1;
+		rs = a * rs;
+	}
+	return rs;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	while (n <= 0) {
+		printf("Invalid input! Please re-enter:\n ");
+		cin >> n;
+	}
+
+	printf("\n%d! = %d", n, Fact(n));
+	
+}
